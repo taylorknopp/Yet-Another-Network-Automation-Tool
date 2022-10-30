d2ff8da0f049e1064548138db263e15f52b14f9d
Taylor Knopp <taylor@knoppfmaily.net>
2022-10-29 23:12:32 -0600

    Updating main py file to newest version, that was originally written for original lab.

---
+++ yanatMain.py
@@ -0,0 +1,75 @@
+
+from classProvider import netDevice
+from classProvider import networkPort
+from classProvider import vlanInterface
+from NetScan import scan
+from SSHutilities import backupConfigsOfDevicesInList
+from SSHutilities import eraseAllDevices
+from JsonUtils import callJsonSave
+import socket
+import os
+
+
+
+def getIpOfHost():
+    hostname=socket.gethostname()   
+    IPAddr=socket.gethostbyname(hostname)
+    return IPAddr
+
+
+menue = '''
+A: Scan And Backup
+B: Wipe Configs And Reload
+What would you like to do?:'''
+print(os.getcwd())
+while True:
+    userInput = input(menue).lower()
+    #scan for devices, add them to the inventory file and backup there configs.
+    if(userInput == "a"):
+        network = "0.0.0.0"
+        try:
+            network = getIpOfHost()
+        except:
+            print("Error Finding IP Of Host Or Network!")
+            continue
+        try:
+            devices = scan(network)
+            #loop to check and remove our pc from the device list
+            for dev in devices:
+                if dev.managementAddress == network:
+                     devices.remove(dev)
+        except Exception as e:
+            print("Error Scanning Network! " + str(e))
+            continue
+        try:
+            devices = backupConfigsOfDevicesInList(devices)
+        except Exception as e:
+            print("Error Backingup Devices! " + str(e))
+            continue
+        try:
+            callJsonSave(devices)
+        except Exception as e:
+            print("Error creating Inventory Files! " + str(e))
+            continue
+    #Scan for devices and wipe them
+    elif(userInput == "b"):
+        userInput = input("Are you sure you want to whipe everyhting(Y to continue, anything else to abort)?: ").lower()
+        try:
+            network = getIpOfHost()
+        except:
+            print("Error Finding IP Of Host OR Network!")
+            continue
+        if(userInput == "y"):
+            try:
+                devices = scan(network)
+                for dev in devices:
+                    if dev.managementAddress == network:
+                        devices.remove(dev)
+                eraseAllDevices(devices)
+            except Exception as e:
+                print("Error Clearing Devices! " + str(e))
+    #quit        
+    elif(userInput == "q"):
+        break
+    else:
+        print("invalid input")
