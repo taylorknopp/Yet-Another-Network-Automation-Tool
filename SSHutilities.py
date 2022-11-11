2a04b8a6e2bd3dc98bade5857bbe2c6cf2d478d0
Taylor Knopp <taylor@knoppfmaily.net>
2022-11-10 17:20:28 -0700

    Setup Scanning, Setup ssh  basic Device info Retrieval. Started on inventory file saving and loading.

--- SSHutilities.py
+++ SSHutilities.py
@@ -13,6 +13,29 @@ def netmikoSendCommandsToDevice(commands: list,Device : netDevice):
         pass
     
 
+def BuildInventoryOfDevicesInList(DeviceList):
+    for CiscoDevice in DeviceList:
+        try:
+            DeviceInfo = {
+            'device_type': 'cisco_ios',
+            'ip': CiscoDevice.managementAddress,
+            'username': 'cisco',
+            'password': 'cisco',
+            'secret': 'cisco',
+            }
+            ssh = netmiko.ConnectHandler(**DeviceInfo)
+            ssh.enable()
+            ShowRun = ssh.send_command('show run')
+            CiscoDevice.config = ShowRun
+            Hostname = ssh.send_command('show run | sec hostname').split()[1]
+            CiscoDevice.hostName = Hostname
+            
+            #print(ShowRun)
+            print(Hostname)
+            ssh.disconnect()
+        except Exception as e:
+            print("Connection Failure For: " + CiscoDevice.managementAddress + " | " + str(e))
+    return DeviceList
 
 
 
--- yanatMain.py
+++ yanatMain.py
@@ -5,11 +5,19 @@ from classProvider import networkPort
 from classProvider import vlanInterface
 from NetOperationsUtils import scan
 from SSHutilities import backupConfigsOfDevicesInList
+from SSHutilities import BuildInventoryOfDevicesInList
 from SSHutilities import eraseAllDevices
 from FileOperationsUtils import saveToInventoryFile
 import socket
 import os
 
+
+
+    
+
+
+listOfDevices = []
+inventoryFile = ""
 
 
 def getIpOfHost():
@@ -25,18 +33,18 @@ def scanNet():
         print("Error Finding IP Of Host Or Network!")
         return
     try:
-        devices = scan(network)
+        devices = scan(network)        
         #loop to check and remove our pc from the device list
         for dev in devices:
             if dev.managementAddress == network:
                     devices.remove(dev)
+        global listOfDevices
+        listOfDevices = devices.copy()
     except Exception as e:
-        print("Error Scanning Network! " + str(e))
+        print("Error scanning network! " + str(e))
         return
-    try:
-        saveToInventoryFile()
-    except Exception as e:
-        print("Error Saving to JSon File: " + e)
+    
+                
 
 def wipeDevices():
     userInput = input("Are you sure you want to wipe everything(Y to continue, anything else to abort)?: ").lower()
@@ -55,10 +63,6 @@ def wipeDevices():
         except Exception as e:
             print("Error Clearing Devices! " + str(e))
             return
-
-def ScanANdBuildInventory():
-    print("san")
-    pass
 
 def uploadConfigs():
     print("upload Config")
@@ -84,20 +88,54 @@ def testConectivity():
     print("test connectivity")
     pass
 
+def BuildInventory():
+    global listOfDevices
+    try:
+        listOfDevices = BuildInventoryOfDevicesInList(listOfDevices).copy()
+    except Exception as e:
+        print("Error Backing up configs! " + str(e))
+
+def InventoryFileSetupAndSave():
+    global inventoryFile
+
+    while True:
+        usrInput = input("Please enter file path or blank to sue the exsiting path: ")
+        if "\\" in usrInput:
+            if usrInput != "" and os.path.exists(usrInput):
+                inventoryFile = usrInput
+                break
+            elif userInput == "" and len(inventoryFile) > 0:
+                break
+            elif userInput == "" and len(inventoryFile) == 0:
+                print("File path/name cannot be blank")
+            else:
+                print("Invalid Input")
+        else:
+            if usrInput == "" and len(inventoryFile) > 0:
+                break
+            elif usrInput == "" and len(inventoryFile) == 0:
+                print("File path/name cannot be blank")
+            elif usrInput != "":
+                inventoryFile = usrInput
+                break
+            else:
+                print("Invalid Input")
+        saveToInventoryFile(listOfDevices,inventoryFile)
 
 
 
-
-menueInputToFunctionMap = {'a':scanNet,'b':wipeDevices, 'c': configureRouting, 'd': backupConfigs, 
-'e': extractConfigs, 'x':wipeConfigs,'y': testConectivity}
+menueInputToFunctionMap = {'a':scanNet,'b':BuildInventory,'c':configureRouting, 'd': backupConfigs, 
+'e': extractConfigs, 'x':wipeConfigs,'y': testConectivity,'s':InventoryFileSetupAndSave}
 
 
 menue = '''
-A: Scan And Build Inventory File
-B: Apply Config Backup To Device or Devices
+A: Scan
+B: Build Inventory
 C: Configure static or Dynamic Routing on a L3 Device
-D: Backup Configurations into Inventory File
+D: Grab Configurations 
 E: Extract configs from inventory
+L: Load Inventory File
+S: Save Inventory File
 X: Wipe Configs And Reload
 Y: Connectivity Test(ping/trace)
 Q: Quit
@@ -105,14 +143,28 @@ What would you like to do?: '''
 
 
 while True:
+    
+    print("Inventory: " + inventoryFile)
+    print("Devices: ")
+    print("==============================================================================")
+    for dev in listOfDevices:
+            print(dev.managementAddress + " | " + dev.hostName)
+    print("==============================================================================")
+
+
+
     userInput = input(menue).lower()
+    
     #scan for devices, add them to the inventory file and backup there configs.
     if(userInput in menueInputToFunctionMap.keys()):
         
         funcToCall = menueInputToFunctionMap[userInput]
         funcToCall()
+        
+
+        
     elif userInput == "q":
-        break;
+        break
     else:
         print("Invaid Input")
         
