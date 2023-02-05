76b845a9cda89654a806261007c5a097862ed236
Taylor Knopp <taylor@knoppfmaily.net>
2023-02-04 22:33:15 -0700

    Manual Console Changes

--- yanatMain.py
+++ yanatMain.py
@@ -524,7 +524,8 @@ def manualConsole():
     print("Choose Port For Multiplexer Control")
     controlPort = openSerialPort()
     time.sleep(5)
-    os.spawnlp(os.P_NOWAIT,"lxterminal","lxterminal -e \"minicom Console\"")
+    minicomCommand = "minicom Console"
+    os.spawnlp(os.P_NOWAIT, f"lxterminal -e {minicomCommand} ")
     selectedIndex = 1
     serialPortToNumberDict = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'j',11:'k',12:'l',13:'m',14:'n',15:'o',16:'p'}
     
