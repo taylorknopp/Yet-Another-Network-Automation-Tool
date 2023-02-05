be53a1fc4bf93cfa4a1a458feb578e14af87eed2
Taylor Knopp <taylor@knoppfmaily.net>
2023-02-04 22:57:59 -0700

    Fixing sub Process command List for manual console

--- yanatMain.py
+++ yanatMain.py
@@ -525,7 +525,7 @@ def manualConsole():
     print("Choose Port For Multiplexer Control")
     controlPort = openSerialPort()
     time.sleep(5)
-    minicomCommand = ["lxterminal","-e","\"minicom","Console\""]
+    minicomCommand = ["lxterminal", "-e", "minicom Console"]
     Popen(minicomCommand)
     selectedIndex = 1
     serialPortToNumberDict = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'j',11:'k',12:'l',13:'m',14:'n',15:'o',16:'p'}
